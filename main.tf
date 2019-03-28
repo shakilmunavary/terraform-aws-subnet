0c3eb2d172eb5482586f56caa3de889207c561b3
shakilmunavary <shakil.ahamed@gmail.com>
2019-03-28 10:42:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,4 +3,6 @@ resource "google_compute_subnetwork" "new_subnetwork" {
   region        = "${var.subnetwork-region}"
   network       = "${var.vpc}"
   ip_cidr_range = "${var.ip_cidr_range}"
+  availability_zone = "${var.availability_zone}"
+  map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
 }
